813bd5fdaf2f82cd0826ea2a1e38af28b3ead62c
sap <hellosudhee@gmail.com>
2015-05-31 21:44:31 +0400

    Basic setup

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import jinja2
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 
+
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/hallo')
 def hello():
-    """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
